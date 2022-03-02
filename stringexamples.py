65ce0960cf7f3a9e7efc89cd2249c8d31911dc14
sridharandharani <sridharandharani1815@gmail.com>
2022-03-02 15:17:35 +0530

    find

--- stringexamples.py
+++ stringexamples.py
@@ -1,9 +1,2 @@
 name = "sridharan"
-new_name="SRIDHARAN"
-greetings = "hello world"
-print(greetings[0:6]+""+name)
-print(name[1])
-print(name[1:5])
-print(name.capitalize())
-print(name.upper())
-print(new_name.lower())
+print(name.find("i"))
