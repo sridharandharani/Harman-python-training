24eb1bf66666a2f4fe3bf92e0c5d9024fbaaec5f
sridharandharani <sridharandharani1815@gmail.com>
2022-03-02 14:34:55 +0530

    type and instance completed

---
+++ instance.py
@@ -0,0 +1,6 @@
+a = 10
+b = "sridharan"
+c = isinstance(a, int)
+d = isinstance(b, str)
+print(c)
+print(d)
---
+++ sample.py
@@ -0,0 +1,6 @@
+a= 10
+b= "sridharan"
+result = type(a)
+output = type(b)
+print(result)
+print(output)
