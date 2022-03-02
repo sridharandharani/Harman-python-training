3817378104fdffe8cbf6cf3f70042b522ffe2aa3
sridharandharani <sridharandharani1815@gmail.com>
2022-03-02 15:10:30 +0530

    capitalize

--- stringexamples.py
+++ stringexamples.py
@@ -1,5 +1,9 @@
 name = "sridharan"
+new_name="SRIDHARAN"
 greetings = "hello world"
 print(greetings[0:6]+""+name)
 print(name[1])
 print(name[1:5])
+print(name.capitalize())
+print(name.upper())
+print(new_name.lower())
