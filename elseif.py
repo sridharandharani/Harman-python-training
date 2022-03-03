e8f0bb8b31efa0307ee7cea43484c075cfabfca4
sridharandharani <sridharandharani1815@gmail.com>
2022-03-03 11:52:40 +0530

    loops

---
+++ elseif.py
@@ -0,0 +1,7 @@
+a = 0
+if a==0:
+    print("its equal to zero")
+elif a >=10:
+    print("its greater than 10")
+else:
+    print("its less thsn zero")
---
+++ oddnumbers.py
@@ -0,0 +1,6 @@
+num = 2
+while num <=50:
+    num += 1
+    if (num % 2 != 0):
+        print("The odd numbers are:" + str(num))
+
