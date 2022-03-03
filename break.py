ab4e78515cd519701c1c2a86c8e865b0e354f829
sridharandharani <sridharandharani1815@gmail.com>
2022-03-03 14:51:51 +0530

    control statements and for loop

---
+++ break.py
@@ -0,0 +1,6 @@
+i = 1
+while i < 10:
+    print(i)
+    if i == 4:
+        break
+    i = i + 1
---
+++ divisibleby8.py
@@ -0,0 +1,3 @@
+#write a program to print all the value in between 2 and 100 which is divisible by 8
+for i in range(2,100,8):
+    print(i)
