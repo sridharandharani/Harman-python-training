27ec8db914d27cf351f82920de03870da85a1fac
sridharandharani <sridharandharani1815@gmail.com>
2022-03-03 13:02:11 +0530

    largest number

---
+++ largest.py
@@ -0,0 +1,11 @@
+first_number = int(input("Enter the first value "))
+second_number = int(input("Enter the second value "))
+third_number = int(input("Enter the third value "))
+if (first_number > second_number):
+ if (first_number > third_number):
+    print("Num1 is larger")
+elif(second_number > third_number) :
+     print("Num2 is larger")
+else:
+    print ("Num3 is larger")
+
